3b3507c3dcf53c447e6db41fdccd8f7b2ca3d62f
Dickson <olejemedickson@gmail.com>
2023-10-14 13:03:47 +0100

    Add a main function and a program menu selection function

--- run.py
+++ run.py
@@ -12,7 +12,47 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('care-plus')
 
-test = SHEET.worksheet('wellness')
+# test = SHEET.worksheet('wellness')
 
-data = test.get_all_values()
-print(data)
+# data = test.get_all_values()
+# print(data)
+
+def start_selection():
+    """
+    Select from menu options
+    """
+    while True:
+        print(" Press 1 - To select existing students")
+        print(" Press 2 - To create new students")
+        print(" Press 3 - To view instructions\n")
+        
+        # main_input = input(int(("Enter: \n"))) 
+        main_input = input("Enter your selection: \n")
+        
+        if main_input == 1:
+            view_students()
+        elif main_input == 2:
+            create_students()
+        elif main_input == 3:
+            view_instructions()
+        else:
+            print("Invalid Selection")
+            start_selection()
+    
+   
+
+def main():
+    """
+    Run all program functions
+    """
+    print("==========================")
+    print("Welcome to Care Plus App\n")
+    print("--------------------------")
+    start_selection()
+    
+
+
+
+main()
+    
+    
