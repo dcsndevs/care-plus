33e695591d037a7b5f44dbad8c8a87b49524d389
Dickson <olejemedickson@gmail.com>
2023-10-19 13:21:09 +0100

    Add instructions text

--- run.py
+++ run.py
@@ -328,9 +328,66 @@ def restart():
             
 def view_instructions():
     """
-    App Instructions3
+    Display Instructions for using the Care Plus App
     """
-    print("This is instruction for how to use this application")
+    welcome = "Welcome to Care Plus App Instructions"
+    print("=" * len(welcome))
+    print(welcome)
+    print("=" * len(welcome))
+    instructions = """
+1. **Select Options:**
+    - Press `1` to view existing students.
+    - Press `2` to create a new student.
+    - Press `3` to view instructions.
+
+2. **View Existing Students:**
+   - After selecting option `1`, you will see a list of existing students.
+   - Enter the name of the student you want to view.
+
+3. **Create a New Student:**
+   - After selecting option `2`, enter the name of the new student.
+   - Follow the prompts to input health and education progress indicators.
+
+4. **View Instructions:**
+   - After selecting option `3`, this screen will be displayed.
+   - These instructions provide guidance on how to use the application effectively.
+
+5. **Progress Entry:**
+   - When entering progress indicators, input values between 0 and 10.
+   - For health and education, the app will visualize the progress with bar charts.
+
+6. **Viewing Student Summary:**
+   - After entering progress indicators, choose to view the student's overall progress.
+   - The app will display the average health and education scores, along with visual representations.
+
+7. **Restart or Exit:**
+   - After completing any operation, press `Enter` to restart the program.
+   - Type 'exit' to terminate the application.
+
+8. **Note:**
+   - Typing Exit in any given input field, would exit the program.
+   - Ensure that you enter valid inputs as guided by the application.
+   - Follow on-screen prompts for a seamless experience.
+
+Enjoy using Care Plus App to manage student data and track their progress!
+    """
+    print(instructions)
+    
+    user_input = input("Enter 'm' to return to the main menu: ")
+    if user_input.lower() == 'm':
+        return_to_main_menu()
+    else:
+        print("Invalid input. Returning to the main menu...\n")
+        return_to_main_menu()
+        
+def return_to_main_menu():
+    """
+    Back to Main menu
+    """
+    print("Returning...\n")
+    start_selection()
+    
+    
     
 
 
